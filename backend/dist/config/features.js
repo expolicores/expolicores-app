187d708a6231cd88319dbd533285e6bcd037f36f
Juan Camilo <expolicores@outlook.com>
2025-11-07 09:01:16 -0500

    reactiving railway.toml v12 09:01 07-11-25

--- backend/dist/config/features.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("@nestjs/config");
-exports.default = (0, config_1.registerAs)('features', () => ({
-    b2b: process.env.FEATURE_B2B === 'true',
-}));
-//# sourceMappingURL=features.js.map
