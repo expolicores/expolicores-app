cc728ca91dd86f57037d39b7f74f09bc048ab15b
Juan Camilo <expolicores@outlook.com>
2025-11-20 17:46:20 -0500

    App 20 Seguridad (Predeploy / Versions Update alert (test change latestVersion 1.1.0.) 20-11-25 17:46

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { LogsModule } from './logs/logs.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AppConfigModule } from './config/app-config.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -51,6 +51,7 @@ import { LiveActivitiesModule } from './live-activities/live-activities.module';
     AddressesModule,
     LogsModule,
     ProductsModule,
+    AppConfigModule,
 
     // Live Activities debe estar disponible para OrdersModule
     LiveActivitiesModule,
